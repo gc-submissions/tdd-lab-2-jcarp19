5dd84f6ace9ed439edc363318dd360aa050c846e
jcarp19 <jim@carpenteronline.us>
2021-07-31 08:43:30 -0400

    final commit

---
+++ src/js/money-functions.js
@@ -0,0 +1,23 @@
+const formatCurrency =(amount) => {
+    return amount >= 0 ? "$" + Number(Math.ceil(amount * 100).toFixed(2)) / 100 : "-$" + Number(Math.ceil(amount * 100).toFixed(2).substring(1)) / 100;
+};
+
+const getCoins = (cents) => {
+    //return an object of coins
+    let change = {quarters: 0, dimes: 0, nickels: 0, pennies: 0};
+    
+    change.quarters = Math.floor(cents / 25); 
+    cents = cents % 25;
+    
+    change.dimes = Math.floor(cents / 10);
+    cents = cents % 10;
+
+    change.nickels = Math.floor(cents / 5);
+    cents = cents % 5;
+
+    change.pennies = Math.floor(cents / 1);
+        
+    return change;
+};
+
+module.exports = {formatCurrency, getCoins};
--- tests/money-functions.test.js
+++ tests/money-functions.test.js
@@ -1,7 +1,68 @@
+const {formatCurrency, getCoins} = require("../src/js/money-functions");
+
 describe("formatCurrency", () => {
-  test.todo("add formatCurrency tests here");
+  test("test1 format currency for positive an negative amounts", () => {
+    expect(formatCurrency(0)).toBe("$" + 0.00);
+  });
+});
+describe("formatCurrency", () => {
+  test("test2 format currency for positive an negative amounts", () => {
+    expect(formatCurrency(1)).toBe("$" + 1.00);
+  });
+});
+describe("formatCurrency", () => {
+  test("test3 format currency for positive an negative amounts", () => {
+    expect(formatCurrency(1.5)).toBe("$" + 1.50);
+  });
+});
+describe("formatCurrency", () => {
+  test("test4 format currency for positive an negative amounts", () => {
+    expect(formatCurrency(0.01)).toBe("$" + 0.01);
+  });
+});
+describe("formatCurrency", () => {
+  test("test5 format currency for positive an negative amounts", () => {
+    expect(formatCurrency(527.6789)).toBe("$" + 527.68);
+  });
+});
+describe("formatCurrency", () => {
+  test("test6 format currency for positive an negative amounts", () => {
+    expect(formatCurrency(-1)).toBe("-$" + 1.00);
+  });
+});
+describe("formatCurrency", () => {
+  test("test7 format currency for positive an negative amounts", () => {
+    expect(formatCurrency(-33)).toBe("-$" + 33.00);
+  });
+});
+describe("formatCurrency", () => {
+  test("test8 format currency for positive an negative amounts", () => {
+    expect(formatCurrency(99.99)).toBe("$" + 99.99);
+  });
+});
+    
+  
+
+describe("getCoins", () => {
+  test("32: testing for change", () => {
+    expect(getCoins(32)).toEqual({"quarters":1, "dimes":0, "nickels":1, "pennies":2});
+  });
 });
 
 describe("getCoins", () => {
-  test.todo("add getCoins tests here");
+  test("10: testing for change", () => {
+    expect(getCoins(10)).toEqual({"quarters":0, "dimes":1, "nickels":0, "pennies":0});
+  });
+});
+
+describe("getCoins", () => {
+  test("27: testing for change", () => {
+    expect(getCoins(27)).toEqual({"quarters":1, "dimes":0, "nickels":0, "pennies":2});
+  });
+});
+
+describe("getCoins", () => {
+  test("68: testing for change", () => {
+    expect(getCoins(68)).toEqual({"quarters":2, "dimes":1, "nickels":1, "pennies":3});
+  });
 });
